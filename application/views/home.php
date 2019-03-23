7e795104881bc114c4b285ce757ec22746f983f2
himanshu-bisht <himanshu-bisht-at-work@users.noreply.github.com>
2019-03-23 13:35:48 +0000

    linked bootstrap to project

--- application/views/home.php
+++ application/views/home.php
@@ -2,22 +2,23 @@
 <html>
 <head>
 	<meta charset="utf-8" />
+	<meta name='RoboGym' content='Welcome to RoboGym!'>
+	<mera name='Author' content='RoboGym'>
+	<title> Welcome to the Homepage of RoboGym</title>
+	<link rel="stylesheet" href="assets/statics/bootstrap/css/bootstrap.min.css">
+  <script src='assets/statics/bootstrap/js/bootstrap.min.js'></script>
 	<style>
 		h1 { text-align: center; 	font-family: Calibri; }
 		p.p-centre { text-align: center; font-family: Arial; }
-		#cogs { display: block; padding-top: 20px; margin-left: auto; margin-right: auto; }		
+		#cogs { display: block; padding-top: 20px; margin-left: auto; margin-right: auto; }
 	</style>
 </head>
-<body>
+<body background=''>
 
-<h1>International Widgets Management System</h1>
+<h1 class='text-success'><strong>Welcome to RoboGym</strong></h1>
 
 <p class="p-centre">This system allows staff at International Widgets to manage stocked items, customers and orders.</p>
 <p class="p-centre">Click one of the navigation links to begin.</p>
 
-<div align="center">
-	<img id="cogs" src="assets/images/cogs.jpg" alt="Cogs and gears" height="260" width="380">
-	<!--Image credits: http://www.wallpaperdecor.com.au/murals/scandinavian-wallpaper-decor/typography-collection/cogs-gears-e21324/ -->
-</div>
 </body>
 </html>

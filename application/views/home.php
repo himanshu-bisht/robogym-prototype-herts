7f82c71ec201075a2ec20e14d8ae329bfcf118bb
himanshu-bisht <himanshu-bisht-at-work@users.noreply.github.com>
2019-03-24 11:58:26 +0000

    [Bug Fix] Removing css for opaque background

--- application/views/home.php
+++ application/views/home.php
@@ -7,24 +7,10 @@
 	<title> Welcome to the Homepage of RoboGym</title>
 	<link rel="stylesheet" href="assets/statics/bootstrap/css/bootstrap.min.css">
   <script src='assets/statics/bootstrap/js/bootstrap.min.js'></script>
-	<style>
-	#background_gym {
-				position: fixed;
-				top: 0;
-				left: 0;
-				width: 100%;
-				height: 100%;
-				background-image: url('assets/images/gym_image.jpg');
-				background-repeat: no-repeat;
-				background-attachment: fixed;
-				background-size: 100%;
-				opacity: 0.5;
-				filter:alpha(opacity=50);
-			}
-	</style>
+
 </head>
 <body>
-	<div id="background_gym">
+
 	<div class='container'>
 		<section class='hero home-bg container-fluid py-5 mt-3 mb-3'>
 		<div class="jumbotron">
@@ -33,7 +19,6 @@
 			<p class="p-centre">Click one of the navigation links to begin.</p>
 		</div>
 	</section>
-	</div>
 	<div>
 </body>
 </html>

64b2d6337788fb2bc75bc051739b78b65961a0f8
himanshu-bisht <himanshu-bisht-at-work@users.noreply.github.com>
2019-03-29 21:41:43 +0000

    [FEATURE] Added login

---
+++ application/views/templates/public_master_view.php
@@ -0,0 +1,7 @@
+2
+3
+4
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+$this->load->view('templates/_parts/public_master_header_view'); ?>
+<?php echo $the_view_content;?>
+<?php $this->load->view('templates/_parts/public_master_footer_view');?>

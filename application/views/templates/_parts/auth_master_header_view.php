0e18568db785580d1b9b32209683a1b05a8fb1dd
himanshu-bisht <himanshu-bisht-at-work@users.noreply.github.com>
2019-04-01 19:10:14 +0100

    login page with appropriate title

--- application/views/templates/_parts/auth_master_header_view.php
+++ application/views/templates/_parts/auth_master_header_view.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title><?php echo $page_title;?></title>
+    <title>RoboGym Login Page</title>
     <meta name="description" value="<?php echo $page_description;?>" />
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <?php echo $before_closing_head;?>

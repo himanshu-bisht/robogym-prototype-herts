8d09aed875464bcb85bf7b3dc5996aff9bb87d9f
himanshu-bisht <himanshu-bisht-at-work@users.noreply.github.com>
2019-04-02 05:27:22 +0100

    [Priyanka] Removed vague table 'Staff Role'

--- application/views/query2_view.php
+++ application/views/query2_view.php
@@ -6,24 +6,24 @@
 		h1, h2 { text-align: center; font-family: Calibri; }
 		table.mytable {border-collapse: collapse;}
 		table.mytable td, th {border: 1px solid grey; padding: 5px 15px 2px 7px;}
-		th {background-color: #f2e4d5;}		
+		th {background-color: #f2e4d5;}
 	</style>
 </head>
 <body>
 
 <h1>Queries</h1>
 <div align='center'>
-	<button type="submit" onclick="location.href='<?php echo site_url('main/query1')?>'">Total customer orders</button>
-	<button type="submit" onclick="location.href='<?php echo site_url('main/query2')?>'">Ranked items by sales</button>
+	<button type="submit" onclick="location.href='<?php echo site_url('main/query1')?>'">Payscale vs Staff</button>
+	<button type="submit" onclick="location.href='<?php echo site_url('main/query2')?>'">Ranked by Goals</button>
 </div>
 <h2>Ranked Items by Sales</h2>
 <div align='center'>
 <?php
 	$tmpl = array ('table_open' => '<table class="mytable">');
-	$this->table->set_template($tmpl); 
-	
+	$this->table->set_template($tmpl);
+
 	$this->db->query('drop table if exists temp');
-	$this->db->query('create temporary table temp as (select order_items.item_id, itemDesc, sum(itemQty) AS TotalQtyOrdered from order_items, items where order_items.item_id = items.itemID group by order_items.item_id order by TotalQtyOrdered DESC)');
+	$this->db->query('create temporary table temp as (select individual_gym_goals.staff_id, staff_type, Count(individual_gym_goals.goal_id) AS TotalGoals from individual_gym_goals, gym_member where gym_member.staff_id = individual_gym_goals.staff_id group by individual_gym_goals.staff_id order by TotalGoals DESC)');
 	$query = $this->db->query('select * from temp;');
 	echo $this->table->generate($query);
 ?>
